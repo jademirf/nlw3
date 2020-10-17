e36e2c50e0c1bff785e7c202b486fa920f388f61
Jademir <jademirf@gmail.com>
2020-10-17 13:52:59 -0300

    adicionando inicial do mobile, configuraçao do expo e navegaçao inicial. Final do dia 04

--- api/src/controllers/OrphanagesController.ts
+++ api/src/controllers/OrphanagesController.ts
@@ -55,7 +55,7 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+      open_on_weekends: open_on_weekends === 'true' ? true : false,
       images
     }
 
@@ -66,7 +66,7 @@ export default {
       about: Yup.string().required().max(300),
       instructions: Yup.string().required(),
       opening_hours: Yup.string().required(),
-      open_on_weekends: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
       images: Yup.array(
         Yup.object().shape({
           path: Yup.string().required()
